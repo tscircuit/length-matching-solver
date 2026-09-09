f33c12c44df987c00e44e05c8881d60a12ebd1ef
mohan-bee <mohn08052006@gmail.com>
2026-09-09 12:16:16 +0530

    up

--- lib/post-processing/geometry/sameCopperPoint.ts
+++ lib/post-processing/geometry/sameCopperPoint.ts
@@ -2,5 +2,4 @@ import type { Point } from "../model/internal-types"
 import { nearlyEqualCopperDimension } from "./nearlyEqualCopperDimension"
 
 export const sameCopperPoint = (a: Point, b: Point): boolean =>
-  nearlyEqualCopperDimension(a.x, b.x) &&
-  nearlyEqualCopperDimension(a.y, b.y)
+  nearlyEqualCopperDimension(a.x, b.x) && nearlyEqualCopperDimension(a.y, b.y)
--- lib/post-processing/geometry/sameCopperSegment.ts
+++ lib/post-processing/geometry/sameCopperSegment.ts
@@ -2,7 +2,10 @@ import type { CopperSegment } from "../model/internal-types"
 import { nearlyEqualCopperDimension } from "./nearlyEqualCopperDimension"
 import { sameCopperPoint } from "./sameCopperPoint"
 
-export const sameCopperSegment = (a: CopperSegment, b: CopperSegment): boolean => {
+export const sameCopperSegment = (
+  a: CopperSegment,
+  b: CopperSegment,
+): boolean => {
   if (a.layer !== b.layer) return false
   if (!nearlyEqualCopperDimension(a.width, b.width)) return false
   if (!sameCopperPoint(a.start, b.start)) return false
